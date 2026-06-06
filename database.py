19dc003a8d06491ebd4f33a079a9f1b8af3c1fac
Ishan Dhakal <ishandh454@gmail.com>
2026-06-06 19:05:56 +0545

    Update database.py

--- database.py
+++ database.py
@@ -1,13 +1,15 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
 
-DATABASE_URL = "postgresql://postgres:ishan123@localhost:5432/authdb"
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:ishan123@localhost:5432/authdb")
+
+if DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
 
 engine = create_engine(DATABASE_URL)
-
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
 Base = declarative_base()
 
 def get_db():
